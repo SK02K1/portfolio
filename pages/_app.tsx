f411c450c917d690a6e01b476ba20b91b1638828
SK02K1 <ksourabh458@gmail.com>
2022-11-03 23:42:14 +0530

    update: change favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Head>
           <link
             rel='icon'
-            href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌈</text></svg>'
+            href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>ॐ</text></svg>'
           />
         </Head>
         <Navbar />
