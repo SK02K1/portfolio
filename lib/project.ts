727fbc16a8bd56ab56170c5cbbff03b80a54cfbd
SK02K1 <ksourabh458@gmail.com>
2022-11-03 22:58:32 +0530

    add: all projects listing

--- lib/project.ts
+++ lib/project.ts
@@ -2,3 +2,8 @@ export const getFeaturedProjects = async () => {
   const data = await import('data/projects/index.json');
   return data.projects.slice(0, 3);
 };
+
+export const getAllProjects = async () => {
+  const data = await import('data/projects/index.json');
+  return data.projects;
+};
