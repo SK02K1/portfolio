c22d8f18634663e2bb6fda49b565c5e93939717a
SK02K1 <ksourabh458@gmail.com>
2022-11-03 23:28:04 +0530

    add: all blogs listing

--- lib/blog.ts
+++ lib/blog.ts
@@ -2,3 +2,8 @@ export const getFeaturedBlogs = async () => {
   const data = await import('data/blogs/index.json');
   return data.blogs.slice(0, 2);
 };
+
+export const getAllBlogs = async () => {
+  const data = await import('data/blogs/index.json');
+  return data.blogs;
+};
